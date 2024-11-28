27be58dda7783695edf3fb010d48061adc5df7c2
Thomas <nipper@wisc.edu>
2024-11-28 11:28:20 -0600

    read in data file and display df

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 import pandas as pd
-from shiny import App, ui, render
+from shiny import App, ui, render, reactive
 
 # UI
 app_ui = ui.page_fluid(
-    ui.input_file("file", "Upload CSV File", accept=[".csv", ".xlsx"], multiple=False),
-    ui.output_table("table")
+    ui.input_file("file", "Upload Data File", accept=[".csv", ".xlsx"], multiple=False),
+    ui.output_data_frame("table")
 )
 
 # Server
 def server(input, output, session):
+    @reactive.calc
+    def df():
+        """ Read in a csv, xlsx, or xls file and return the dataframe for display and analysis."""
+        file_info = input.file()
+        if file_info is not None:
+            file_path = file_info[0]["datapath"]
+            if file_path.endswith(".csv"):
+                df = pd.read_csv(file_path)
+            elif file_path.endswith(".xlsx"):
+                df = pd.read_excel(file_path)
+            elif file_path.endswith(".xls"):
+                df = pd.read_excel(file_path)
+            return df
+    @output
     @render.data_frame
-    def summary():
-        if ".csv" in input.file:
-            df = pd.read_csv(input.file)
-        elif ".xlsx" in input.file:
-            df = pd.read_excel(input.file)
-        return render.data_frame
+    def table():
+        return df()
 
 
 app = App(app_ui, server)
